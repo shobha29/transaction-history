14aa70966bc651c23550d64d5597313d1cd7a164
shobha29 <shobhashrivastava1998@gmail.com>
2021-05-29 18:41:07 +0530

    group by date

--- src/screens/transactionHistory/index.js
+++ src/screens/transactionHistory/index.js
@@ -1,8 +1,14 @@
 import React, { useState, useEffect } from "react";
 import moment from "moment";
 import axios from "axios";
+import _ from "lodash";
 
-import { forwardArrow, exclamationMark, checkMark } from "../../asserts";
+import {
+  forwardArrow,
+  exclamationMark,
+  checkMark,
+  backArrow,
+} from "../../asserts";
 
 import "./styles.scss";
 
@@ -17,90 +23,112 @@ const TransactionHistory = (props) => {
         "https://dev.onebanc.ai/assignment.asmx/GetTransactionHistory?userId=1&recipientId=2",
     })
       .then((res) => {
-        setTransactionList(res.data.transactions);
+        const data = res.data.transactions;
+        setTransactionList(
+          Object.values(
+            _.groupBy(data, (item) =>
+              moment(item.startDate).format("DD MM YYYY")
+            )
+          )
+        );
       })
       .catch((err) => {
         alert("Error while fetching data");
-        console.log("err is ", err);
       });
   }, []);
 
   const Card = (item) => {
     const transaction = item.item;
 
-    const transactionDate = new Date(transaction.startDate).getTime();
-
+    const transactionDate = new Date(transaction[0].startDate).getTime();
     return (
-      <div
-        className={
-          transaction.direction === 1
-            ? "card-container right-card"
-            : "card-container left-card"
-        }
-      >
-        <div className="card">
-          <div className="top">
-            <p className="top-left">{`₹ ${transaction.amount}`}</p>
-
-            {transaction.status === 1 ? (
-              <div className="top-right">
-                <img src={exclamationMark} alt="forward-arrow" />
-                <p>
-                  {transaction.direction === 1
-                    ? "You requested"
-                    : "Request received"}
-                </p>
-              </div>
-            ) : (
-              <div className="top-right">
-                <img src={checkMark} alt="forward-arrow" />
-                <p>
-                  {transaction.direction === 1 ? "You paid" : "You received"}
-                </p>
-              </div>
-            )}
-          </div>
-
-          <div className="bottom">
-            {transaction.status === 1 ? (
-              transaction.direction === 1 ? (
-                <div className="btn-container">
-                  <div className="btn">
-                    <p>Pay</p>
-                  </div>
-                  <div className="btn">
-                    <p>Decline</p>
-                  </div>
-                </div>
-              ) : (
-                <div className="btn">
-                  <p>Cancel</p>
-                </div>
-              )
-            ) : (
-              <div className="transaction-id">
-                <p>Transaction ID</p>
-                <p>A1234156256787382783899</p>
-              </div>
-            )}
-            <img
-              src={forwardArrow}
-              alt="forward-arrow"
-              onClick={() =>
-                history?.push("/transaction-summary", { transaction })
-              }
-            />
-          </div>
+      <>
+        <div className="date">
+          <div className="horizontal-line left-line" />
+          <p>{moment(transactionDate).format("D MMM YYYY")}</p>
+          <div className="horizontal-line right-line" />
         </div>
-        <div className="transaction-date-time">
-          <p>{moment(transactionDate).format("DD MMM YYYY, hh:mm A")}</p>
-        </div>
-      </div>
+        {transaction.map((data) => (
+          <div
+            className={
+              data.direction === 1
+                ? "card-container right-card"
+                : "card-container left-card"
+            }
+          >
+            <div className="card">
+              <div className="top">
+                <p className="top-left">{`₹ ${data.amount}`}</p>
+
+                {data.status === 1 ? (
+                  <div className="top-right">
+                    <img src={exclamationMark} alt="forward-arrow" />
+                    <p>
+                      {data.direction === 1
+                        ? "You requested"
+                        : "Request received"}
+                    </p>
+                  </div>
+                ) : (
+                  <div className="top-right">
+                    <img src={checkMark} alt="forward-arrow" />
+                    <p>{data.direction === 1 ? "You paid" : "You received"}</p>
+                  </div>
+                )}
+              </div>
+
+              <div className="bottom">
+                {data.status === 1 ? (
+                  data.direction === 1 ? (
+                    <div className="btn-container">
+                      <div className="btn">
+                        <p>Pay</p>
+                      </div>
+                      <div className="btn">
+                        <p>Decline</p>
+                      </div>
+                    </div>
+                  ) : (
+                    <div className="btn">
+                      <p>Cancel</p>
+                    </div>
+                  )
+                ) : (
+                  <div className="transaction-id">
+                    <p>Transaction ID</p>
+                    <p>A1234156256787382783899</p>
+                  </div>
+                )}
+                <img
+                  src={forwardArrow}
+                  alt="forward-arrow"
+                  onClick={() =>
+                    history?.push("/transaction-summary", { data })
+                  }
+                />
+              </div>
+            </div>
+            <div className="transaction-date-time">
+              <p>{moment(transactionDate).format("DD MMM YYYY, hh:mm A")}</p>
+            </div>
+          </div>
+        ))}
+      </>
     );
   };
 
   return (
     <div className="container">
+      <div className="header">
+        <img src={backArrow} alt="back arrow" />
+        <div className="user-detail">
+          <div className="profile">J</div>
+          <div className="name-phone">
+            <p className="name">John Deo</p>
+            <p className="phone">+91 7672 2345</p>
+          </div>
+        </div>
+      </div>
       {transactionList.length !== 0 &&
         transactionList.map((item) => <Card item={item} />)}
     </div>
